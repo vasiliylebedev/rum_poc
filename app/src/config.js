cb430769ff9043094a1985564bb8991c2f9bec8b
Василий Лебедев <v.lebedev@corp.kaiten.ru>
2026-03-26 18:17:32 +0700

    Server url fix

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
 //const API_BASE = 'http://localhost:4000';
-const API_BASE = 'http://pepe-watafa.ru:4000/api/card';
+const API_BASE = 'http://pepe-watafa.ru:4000';
 const CARD_URL = `${API_BASE}/api/card`;
 const CARD_EXTRA_URL = `${API_BASE}/api/cards/extra`;
 
